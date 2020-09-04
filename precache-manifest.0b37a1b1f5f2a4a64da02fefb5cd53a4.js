70fd84a48f548d4890992e35c1811b18e58ea566
nikki <rssnikki@gmail.com>
2020-09-04 16:50:18 +0500

    Updates

---
+++ precache-manifest.0b37a1b1f5f2a4a64da02fefb5cd53a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a87bcb59863eca6551f2aaf0c1e9740",
+    "url": "/new-aeroset-pwa/index.html"
+  },
+  {
+    "revision": "bf47ef2277727e887f36",
+    "url": "/new-aeroset-pwa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c536a6ea69df63e6453f",
+    "url": "/new-aeroset-pwa/static/js/2.6419052e.chunk.js"
+  },
+  {
+    "revision": "98c87275776474e5a317cb0df95722c7",
+    "url": "/new-aeroset-pwa/static/js/2.6419052e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf47ef2277727e887f36",
+    "url": "/new-aeroset-pwa/static/js/main.f1d8ab4f.chunk.js"
+  },
+  {
+    "revision": "2fca51e1ce404144a6c5",
+    "url": "/new-aeroset-pwa/static/js/runtime-main.ae2fa218.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-aeroset-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
