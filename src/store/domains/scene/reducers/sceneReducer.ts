0dec9bbadcdec763b921448014cec20000b92ae1
nikki <rssnikki@gmail.com>
2020-10-13 13:04:27 +0500

    feat: remove garbage

--- src/store/domains/scene/reducers/sceneReducer.ts
+++ src/store/domains/scene/reducers/sceneReducer.ts
@@ -3,13 +3,7 @@ import {createAction, createReducer} from "@reduxjs/toolkit";
 
 const defaultState: SceneState = {};
 
-export const tempAction = createAction<number>("TEMP");
-
 const sceneReducer = createReducer(defaultState, builder => {
-    builder.addCase(tempAction, (state, action) => {
-        alert("sceneReducer");
-        return state;
-    });
 });
 
 export default sceneReducer;
