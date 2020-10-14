fe52c51b46177982bbccf56006f7bb185ff14a70
nikki <rssnikki@gmail.com>
2020-10-14 22:19:47 +0500

    feat: install lodash

--- src/store/domains/instrumentalPanel/sections/pointCloudSystemSection/reductors/pointCloudSystemSectionReducer.ts
+++ src/store/domains/instrumentalPanel/sections/pointCloudSystemSection/reductors/pointCloudSystemSectionReducer.ts
@@ -14,13 +14,14 @@ export const closePcsFiltersPanelAction = createAction("POINT_CLOUD_SYSTEM_SECTI
 
 const pointCloudSystemSectionReducer = createReducer(defaultState, builder => {
     builder.addCase(pcsLoadFileAction, (state, action) => {
-        console.log("call pointCloudSystemSectionReducer");
         return update(state, {
             pointsCloudFile: {$set: action.payload}
         });
     });
     builder.addCase(showPcsFiltersPanelAction, (state, action) => {
+        return update(state, {
 
+        })
     });
     builder.addCase(closePcsFiltersPanelAction, (state, action) => {
 
