96b7c0a45051603c63d58a2a52a33f79e50717c1
nikki <rssnikki@gmail.com>
2020-12-08 17:38:14 +0500

    feat: drawing point from scheme file

--- src/views/components/layout/scheme/code/constructTopologyScheme.ts
+++ src/views/components/layout/scheme/code/constructTopologyScheme.ts
@@ -1,40 +1,40 @@
 import Scheme from "../../../../../models/scheme/Scheme";
 import {Scene} from "@babylonjs/core/scene";
-import {MeshBuilder, SolidParticleSystem, Vector3} from "@babylonjs/core";
+import {Color4, MeshBuilder, SolidParticleSystem, Vector3} from "@babylonjs/core";
 
 export const constructTopologyScheme = async (scheme: Scheme, scene: Scene): Promise<void> => {
-    const sceneSPS: SolidParticleSystem = new SolidParticleSystem("SceneSPS", scene);
+    const sceneSPS: SolidParticleSystem = new SolidParticleSystem("SceneSPS", scene, {
+        updatable: true,
+        isPickable : true
+    });
 
     for (const node of scheme.nodes) {
-        const point = node.point;
-        const nodeVector = new Vector3(point.x, point.y, point.z)
         const nodeId = node.id.toString();
-
         const sphere = MeshBuilder.CreateSphere(`sphere, id=${nodeId}`,
             {
-                diameter: 2
+                diameter: 0.25
             }, scene);
-
-        sphere.position = nodeVector
 
         sceneSPS.addShape(sphere, 1)
         sphere.dispose()
     }
 
     sceneSPS.buildMesh()
-    // sceneSPS.initParticles = function () {
-    //     const node = sceneSPS.particles
-    //     const point = node.point;
-    //     const nodeVector = new Vector3(point.x, point.y, point.z)
-    //     SPS.particles[0].color = new BABYLON.Color4(1, 1, 1, 1);
-    //     for (var p = 1; p < SPS.nbParticles; p++) {
-    //         myPositionFunction(SPS.particles[p]);
-    //     }
-    // }
-    // sceneSPS.updateParticle = () => {
-    //     SPS.particles[0].color = new BABYLON.Color4(1, 1, 1, 1);
-    //     for (var p = 1; p < SPS.nbParticles; p++) {
-    //         myPositionFunction(SPS.particles[p]);
-    //     }
-    // }
+
+
+    sceneSPS.initParticles = function () {
+        for (let i = 0; i < sceneSPS.nbParticles; i++) {
+            const node = scheme.nodes[i]
+
+            const point = sceneSPS.particles[i]
+            point.position.x = node.point.x
+            point.position.y = node.point.y
+            point.position.z = node.point.z
+
+            point.color = new Color4(3, 3, 3, 1)
+        }
+    }
+    sceneSPS.initParticles();
+    sceneSPS.setParticles();
+    sceneSPS.refreshVisibleSize();
 }
