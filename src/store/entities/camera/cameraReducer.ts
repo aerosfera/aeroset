c12b2edb70b5551d20ef7346654bba31e4c11bc9
nikki <rssnikki@gmail.com>
2021-01-22 12:04:48 +0500

    feat: add cameraMode interface

--- src/store/entities/camera/cameraReducer.ts
+++ src/store/entities/camera/cameraReducer.ts
@@ -2,13 +2,16 @@ import {createSlice, PayloadAction, Selector} from "@reduxjs/toolkit";
 import {ApplicationState} from "../../store";
 import produce from "immer";
 import {Vector3} from "@babylonjs/core";
+import {CameraMode} from "../../../views/types/CameraMode";
 
 export interface CameraState {
     target: Vector3
+    mode: CameraMode
 }
 
 const defaultState: CameraState = {
-    target: new Vector3()
+    target: new Vector3(),
+    mode: CameraMode.Orthographic
 }
 
 const slice = createSlice({
@@ -19,13 +22,21 @@ const slice = createSlice({
             return produce(state, (draft) => {
                 draft.target = action.payload
             });
+        },
+        cameraModeChanged(state: CameraState, action: PayloadAction<CameraMode>) {
+            return produce(state, (draft) => {
+                draft.mode = action.payload
+            });
         }
     }
 });
 
 
-export const cameraTargetChangedSelector: Selector<ApplicationState, Vector3> =
+export const cameraTargetSelector: Selector<ApplicationState, Vector3> =
     state => state.entities.camera.target;
+
+export const cameraTargetModeSelector: Selector<ApplicationState, CameraMode> =
+    state => state.entities.camera.mode;
 
 const {actions, reducer} = slice;
 export const {cameraTargetChanged} = actions;
