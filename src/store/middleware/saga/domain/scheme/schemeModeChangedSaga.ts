1f628d74b1bdbea6bb64928610808df649b794b5
nikki <rssnikki@gmail.com>
2021-02-09 16:55:22 +0500

    fix: add additional cleanup when changing scheme mode

--- src/store/middleware/saga/domain/scheme/schemeModeChangedSaga.ts
+++ src/store/middleware/saga/domain/scheme/schemeModeChangedSaga.ts
@@ -1,7 +1,8 @@
 import {SchemeMode} from "../../../../../views/types/SchemeMode";
 import {put} from "redux-saga/effects";
-import {closeSchemeModelsPanel} from "../../../../ui/panels/models/schemeModelsPanel";
+import {closeSchemeModelsPanel, setActiveModel} from "../../../../ui/panels/models/schemeModelsPanel";
 import {setActiveModelId} from "../../../../domain/scheme/activeSchemeReducer";
+import {SchemeModelType} from "../../../../../views/types/SchemeModelType";
 
 export function* schemeModeChangedSaga(action: { payload: SchemeMode }) {
     const schemeMode = action.payload;
@@ -10,6 +11,7 @@ export function* schemeModeChangedSaga(action: { payload: SchemeMode }) {
         case SchemeMode.Topology:
             yield put(closeSchemeModelsPanel());
             yield put(setActiveModelId(null));
+            yield put(setActiveModel(SchemeModelType.None));
             break;
         case SchemeMode.RibGeometry:
             break;
