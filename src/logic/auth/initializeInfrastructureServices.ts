3390fd309c2e66634bb9436c1ebc1f8406f7016f
nikki <rssnikki@gmail.com>
2021-04-11 17:16:51 +0500

    feat: add some initialization when auth

--- src/logic/auth/initializeInfrastructureServices.ts
+++ src/logic/auth/initializeInfrastructureServices.ts
@@ -1,6 +1,6 @@
 import {META_DB_CONNECTION_STRING} from "../../config/connection";
 import IoC from "../../infrastructure/ioc/IoC";
-import {EVENT_BUS_SERVICE, REPLICATION_SERVICE} from "../../infrastructure/ioc/ServiceTypes";
+import { REPLICATION_SERVICE} from "../../infrastructure/ioc/ServiceTypes";
 import ReplicationService from "../../services/replicationService/ReplicationService";
 
 export function initializeInfrastructureServices(organization: string, token: string){
